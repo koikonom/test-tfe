5050d6f82f39ff102fd8eca89c36322b90f1bf03
Kyriakos Oikonomakos <kyriakos@fastmail.com>
2020-08-26 19:03:39 +0100

    .

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
-#terraform {
-#  required_providers {
-#    tfe = {
-#      source  = "hashicorp/tfe"
-#      version = "0.21.0"
-#    }
-#  }
-#}
+terraform {
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "0.21.0"
+    }
+  }
+}
 
 provider "tfe" {}
 
 resource "tfe_organization" "test" {
-  name  = "aaaa-not-so-random-org-name"
+  name  = "a-not-so-random-org-name"
   email = "admin@company.com"
 }
 
